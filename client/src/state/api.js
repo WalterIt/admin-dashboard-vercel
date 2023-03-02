d9e554e743778e26bfbe173d3745c276cbff6e24
Valto Silva <valtosilva7@gmail.com>
2023-03-02 16:13:19 -0300

    First commit ready to deploy

---
+++ client/src/state/api.js
@@ -0,0 +1,71 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  reducerPath: "adminApi",
+  tagTypes: [
+    "User",
+    "Products",
+    "Customers",
+    "Transactions",
+    "Geography",
+    "Sales",
+    "Admins",
+    "Performance",
+    "Dashboard",
+  ],
+  endpoints: (build) => ({
+    getUser: build.query({
+      query: (id) => `api/general/user/${id}`,
+      providesTags: ["User"],
+    }),
+    getProducts: build.query({
+      query: () => "api/client/products",
+      providesTags: ["Products"],
+    }),
+    getCustomers: build.query({
+      query: () => "api/client/customers",
+      providesTags: ["Customers"],
+    }),
+    getTransactions: build.query({
+      query: ({ page, pageSize, sort, search }) => ({
+        url: "api/client/transactions",
+        method: "GET",
+        params: { page, pageSize, sort, search },
+      }),
+      providesTags: ["Transactions"],
+    }),
+    getGeography: build.query({
+      query: () => "api/client/geography",
+      providesTags: ["Geography"],
+    }),
+    getSales: build.query({
+      query: () => "api/sales/sales",
+      providesTags: ["Sales"],
+    }),
+    getAdmins: build.query({
+      query: () => "api/management/admins",
+      providesTags: ["Admins"],
+    }),
+    getUserPerformance: build.query({
+      query: (id) => `api/management/performance/${id}`,
+      providesTags: ["Performance"],
+    }),
+    getDashboard: build.query({
+      query: () => "api/general/dashboard",
+      providesTags: ["Dashboard"],
+    }),
+  }),
+});
+
+export const {
+  useGetUserQuery,
+  useGetProductsQuery,
+  useGetCustomersQuery,
+  useGetTransactionsQuery,
+  useGetGeographyQuery,
+  useGetSalesQuery,
+  useGetAdminsQuery,
+  useGetUserPerformanceQuery,
+  useGetDashboardQuery,
+} = api;
